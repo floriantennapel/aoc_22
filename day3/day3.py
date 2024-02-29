1363d4b75e4a78948579e8f06ea5d20cfe442239
Florian ten Napel <tennapel.f@gmail.com>
2024-03-01 00:11:27 +0100

    finished day3

---
+++ day3/day3.py
@@ -0,0 +1,59 @@
+input = open('input.txt')
+lines = input.readlines()
+input.close()
+
+
+def getPriority(c):
+    if c.islower():
+        return ord(c) - ord('a') + 1
+    else:
+        return ord(c) - ord('A') + 27
+
+
+def part1():
+    sum = 0
+    for line in lines:
+        halfway = int(len(line) / 2)
+        fst_half = line[:halfway]
+        snd_half = line[halfway:-1] #excluding newline
+
+        for c in fst_half:
+            if c in snd_half:
+                sum += getPriority(c)
+                break
+
+    return sum
+
+
+def part2():
+    sum = 0
+    
+    group_index = 0
+    while group_index < len(lines):
+        fst = lines[group_index]
+        snd = lines[group_index + 1]
+        trd = lines[group_index + 2]
+        
+        matches = []
+        for c in fst:
+            if c in snd: 
+                matches.append(c)
+
+        for c in matches:
+            if c in trd:
+                sum += getPriority(c)
+                break
+
+        group_index += 3
+
+    return sum
+
+
+print(part1())
+print(part2())
+
+
+
+
+
+
