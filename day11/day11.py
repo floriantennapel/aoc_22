e89cefe6a155b2fd1efc4e1293359c2558a6ab06
Florian ten Napel <tennapel.f@gmail.com>
2024-03-04 14:15:24 +0100

    Finished day 11
    I did not feel that I had the knowledge to figure out part 2 by myself, so I did do a little digging online to figure out a way to solve the problem.

---
+++ day11/day11.py
@@ -0,0 +1,108 @@
+# I did need some help to find out what to do in part 2
+# this is the product of all divisors used in monkey tests
+lcm = 17 * 3 * 5 * 7 * 11 * 19 * 2 * 13
+
+
+class Monkey:
+    def __init__(self, items, operation, test, part1):
+        self.items = items
+        self.operation = operation
+        self.test = test
+        self.inspections = 0
+
+        # are we solving part 1 or 2?
+        self.part1 = part1
+
+    def get_item(self, item):
+        self.items.append(item)
+
+    def inspect_item(self):
+        self.inspections += 1
+        item = self.items.pop(0)
+
+        if self.part1:
+            item = self.operation(item) // 3
+        else:
+            item = self.operation(item) % lcm
+
+        next_monkey = self.test(item)
+        return (item, next_monkey)
+
+
+def parse_test(lines):
+    split_lines = [line.split() for line in lines]
+    divisor = int(split_lines[0][-1])
+    t = int(split_lines[1][-1])
+    f = int(split_lines[2][-1])
+
+    return lambda n: t if n % divisor == 0 else f
+
+
+def parse_operation(line):
+    # monkey 7
+    if line.split()[-1] == "old":
+        return lambda n: n * n
+
+    op = line[20:]
+    return lambda n: eval(str(n) + op)
+
+
+def parse_input(file_name, part1=True):
+    file = open(file_name)
+    lines = [line.strip() for line in file.readlines()]
+    file.close()
+
+    monkeys = []
+
+    # defining monkey parameters
+    items = []
+    operation = lambda: None
+    test = lambda: None
+
+    i = 0
+    while i < len(lines):
+        line = lines[i]
+        split = line.split()
+
+        match split[0]:
+            case "Starting":
+                items = [int(item.strip(",")) for item in split[2:]]
+            case "Operation:":
+                operation = parse_operation(line)
+            case "Test:":
+                test = parse_test([lines[j] for j in range(i, i + 3)])
+                i += 3
+                monkeys.append(Monkey(items, operation, test, part1))
+
+        i += 1
+
+    return monkeys
+
+
+def monkey_round(monkeys):
+    for monkey in monkeys:
+        for _ in range(len(monkey.items)):
+            item, next_monkey_index = monkey.inspect_item()
+            monkeys[next_monkey_index].get_item(item)
+
+
+def monkey_business(rounds, monkeys):
+    for _ in range(rounds):
+        monkey_round(monkeys)
+
+    sort_monkeys = sorted([monkey.inspections for monkey in monkeys])
+    return sort_monkeys[-1] * sort_monkeys[-2]
+
+
+def part1():
+    monkeys = parse_input("input.txt")
+    return monkey_business(20, monkeys)
+
+
+def part2():
+    monkeys = parse_input("input.txt", False)
+    return monkey_business(10000, monkeys)
+
+
+print(part1())
+print(part2())
