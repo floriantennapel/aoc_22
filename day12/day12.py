7df794aa4b9c305bb93f7bc85b9a30da61a5d051
Florian ten Napel <tennapel.f@gmail.com>
2024-03-15 03:07:15 +0100

    reformatted code

--- day12/day12.py
+++ day12/day12.py
@@ -4,29 +4,33 @@ file = open('input.txt')
 hills = [line.strip() for line in file.readlines()]
 file.close()
 
+
 class HillClimb:
     def __init__(self, pos, prev):
         self.pos = pos
         self.steps = prev.steps + 1 if prev is not None else 0
         self.height = hills[pos[0]][pos[1]]
 
+
 def valid_step(prev_height, new_height):
     if new_height == 'E':
         return prev_height in 'zy'
     if prev_height == 'S':
         return new_height in 'ab'
-    
+
     return ord(new_height) - ord(prev_height) <= 1
+
 
 def valid_pos(pos):
     r = pos[0]
     c = pos[1]
     return r >= 0 and r < len(hills) and c >= 0 and c < len(hills[0])
 
+
 def get_next(prev, visited, part1):
     next = set()
-    
-    for d_r, d_c in [(1,0), (-1,0), (0,1), (0,-1)]:
+
+    for d_r, d_c in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
         p = prev.pos
         new_pos = (p[0] + d_r, p[1] + d_c)
         if not valid_pos(new_pos):
@@ -35,7 +39,7 @@ def get_next(prev, visited, part1):
         new_height = hills[new_pos[0]][new_pos[1]]
         from_height = prev.height if part1 else new_height
         to_height = new_height if part1 else prev.height
-        
+
         if new_pos not in visited and valid_step(from_height, to_height):
             next_hill = HillClimb(new_pos, prev)
             next.add(next_hill)
@@ -43,11 +47,13 @@ def get_next(prev, visited, part1):
 
     return next
 
+
 def find_char(c):
     for i, row in enumerate(hills):
         for j, col in enumerate(row):
             if c == col:
                 return (i, j)
+
 
 def bfs(part1):
     visited = set()
@@ -67,7 +73,6 @@ def bfs(part1):
         for hill_climb in get_next(current, visited, part1):
             queue.put(hill_climb)
 
+
 bfs(True)
 bfs(False)
-
-
